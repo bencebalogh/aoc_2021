d60dff3ce4bbb9c856d3fe9efb8cd1c171b27953
Bence Balogh <kapa.daniel@gmail.com>
2021-12-24 12:49:05 +0000

    day23 and 24 - done with pen and paper

---
+++ 23/aoc.erl
@@ -0,0 +1,10 @@
+-module(aoc).
+-export([part1/0,part2/0]).
+
+% Worked out with pen and paper
+
+part1() ->
+    14148.
+
+part2() ->
+    43814.
---
+++ 24/aoc.erl
@@ -0,0 +1,10 @@
+-module(aoc).
+-export([part1/0,part2/0]).
+
+% Worked out on with and paper, after working out what the input does.
+
+part1() ->
+    92793949489995.
+
+part2() ->
+    51131616112781.
